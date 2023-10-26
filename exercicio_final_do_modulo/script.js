340ff107100fce2dcd4ff325a961ef24fab863e6
Rodrigo Carvalho <rodrigo.software42@gmail.com>
2023-10-26 03:28:37 -0300

    fix bugs

--- exercicio_final_do_modulo/script.js
+++ exercicio_final_do_modulo/script.js
@@ -1,34 +1,49 @@
 // animação menu mobile
+
 const ulMobile = document.querySelector("header > nav > ul")
 const liMobile = document.querySelectorAll("header > nav > ul > li ")
 const barMobile = document.querySelector("#navMobile")
 const logo = document.querySelector("#logo")
 const navMobile = document.querySelector("#navMobile")
-navMobile.addEventListener("click", () => {
 
-    ulMobile.style.flexDirection = "column"
-    barMobile.style.display = "none"
-    liMobile.forEach((e) => {
-        e.style.display = "flex"
-    })
-    ulMobile.classList.toggle("ulMobile")
-})
-const linksNav = document.querySelectorAll("header > nav > ul > li > a")
-linksNav.forEach(element => {
-    element.addEventListener("click", fechaMobile )
-});
-const exitNavMobile = document.querySelector("#endnavmobile")
-exitNavMobile.addEventListener("click", (e) => {
-    ulMobile.style.flexDirection = "row"
-    barMobile.style.display = "flex"
-    liMobile.forEach((e) => {
-        e.style.display = "none"
-    })
-    ulMobile.classList.toggle("ulMobile")
+window.addEventListener("resize", () => verifica())
+window.addEventListener("load", () => verifica())
 
-    logo.style.display = "flex"
-    navMobile.parentElement.style.display = " flex"
-})
+function verifica() {
+        // verifica o tamanho da width 
+if(window.screen.width < 500){
+
+
+    navMobile.addEventListener("click", () => {
+    
+        ulMobile.style.flexDirection = "column"
+        barMobile.style.display = "none"
+        liMobile.forEach((e) => {
+            e.style.display = "flex"
+        })
+        ulMobile.classList.toggle("ulMobile")
+    })
+    const linksNav = document.querySelectorAll("header > nav > ul > li > a")
+    linksNav.forEach(element => {
+        element.addEventListener("click", fechaMobile )
+    });
+    const exitNavMobile = document.querySelector("#endnavmobile")
+    exitNavMobile.addEventListener("click", (e) => {
+        ulMobile.style.flexDirection = "row"
+        barMobile.style.display = "flex"
+        liMobile.forEach((e) => {
+            e.style.display = "none"
+        })
+        ulMobile.classList.toggle("ulMobile")
+    
+        logo.style.display = "flex"
+        navMobile.parentElement.style.display = " flex"
+    })
+    
+    }else{
+        console.log("a")
+    }
+}
 
 function fechaMobile() {
     ulMobile.style.flexDirection = "row"
