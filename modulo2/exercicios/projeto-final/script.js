51e7af25bb4449b794f8cd9ab996040e4cc5bc66
Rodrigo Silva <89227598+RodrigoSilva5@users.noreply.github.com>
2023-11-25 02:45:20 -0300

    Update script.js

--- modulo2/exercicios/projeto-final/script.js
+++ modulo2/exercicios/projeto-final/script.js
@@ -1,22 +1,54 @@
-// 
-let dados = [{
-    "id":"",
-    "titulo":"",
-    "descricao":""
-},
-{
-    "id":123,
-    "titulo":"",
-    "descricao":""
-}]
+let dados = [
+//     {
+//     "id":"524",
+//     "titulo":"algo1",
+//     "descricao":"lorem uahfshjfhjhfhdsfbgghnmggngthtttt"
+// },
+// {
+//     "id":"564",
+//     "titulo":"algo4",
+//     "descricao":"lorem uahfshjfhjhfhdsfbgghnmggngthtttt"
+// },
+// {
+//     "id":"64",
+//     "titulo":"algo5",
+//     "descricao":"lorem uahfshjfhjhfhdsfbgghnmggngthtttt"
+// }
+]
 
 // criar
 function criar_tarefa(titulo, descricao) {
+    if(titulo === "" ||  descricao === ""){
+        // verifica  se titulo ou descriçao vazios
+        console.log(`titulo ou descricao vazios, titulo:${titulo},descrição:${descricao}`)
+        return
+    }
+    if(titulo.match(/^[0-9]+$/) != null){
+        //verifica se somente tem numeros 
+        console.log("titulo somente com numeros")
+        return
+    }
+    if(titulo.length < 4){
+        // verifica se titulo tem menos que 4 caracteres
+        console.log("titulo com menos de 4 caracteres")
+        return
+    }
+    if(descricao.length < 20){
+        // verifica se descrica tem menos que 20 caracteres
+        console.log("descricao com menos de 20 caracteres")
+        return
+    }
+    if(dados.findIndex(x => x.titulo === titulo) !== -1){
+        // verifica se titulo ja existe
+        console.log("titulo ja existente")
+        return
+    }
     const id = Math.floor( Math.random() * 1000)
-    // 
+
     dados.push(
         {id,titulo,descricao}
     )
+    escreve_tabela(dados)
     return id
 }
 // ler 
@@ -30,7 +62,9 @@ function deletar_tarefa(id) {
     }else{
         dados.splice(dados.findIndex(x => x.id === id), 1)
         console.log("deletado com sucesso", dados)
+        escreve_tabela(dados)
     }
+
 
 }
 // atualizar
@@ -50,4 +84,26 @@ function atualizar_tarefa(id, titulo, descricao) {
     }
 
 }
-console.log(atualizar_tarefa(123,"", "descricao muito grande"), dados)
+// renderiza dados na tabela
+function escreve_tabela(dados) {
+    const tbody = document.querySelector("#tbody")
+    document.querySelectorAll("tbody").forEach(a => a.innerHTML = "")
+    dados.forEach(dado => {
+        const th = `
+                 <tr>
+                  <td>${dado.id}</td>
+                  <td>${dado.titulo}</td>
+                  <td>${dado.descricao}</td>
+                  <td>               
+                 <button class="btn btn-danger" onclick="deletar_tarefa(${dado.id})"  ng-click="delete($index)">
+                    Delete
+                  </button>
+
+                  </td>
+              </tr>
+    `  
+    tbody.insertAdjacentHTML("afterend", th)  
+    })
+
+}
+// console.log()
