d4ec4b359e185dec62dffa6439bd232dd16d3485
Rodrigo Silva <89227598+RodrigoSilva5@users.noreply.github.com>
2023-12-19 00:55:47 -0300

    Update lista-easy.js

--- modulo3/projeto_final/lista-easy.js
+++ modulo3/projeto_final/lista-easy.js
@@ -24,23 +24,33 @@ class Hotel{
     }
 
     confirmarReserva(numero, hospede){
-        const buscaQuarto = this.lista_de_quartos.find((quarto)=> quarto.numero === numero)
-       if(buscaQuarto === undefined){
-           console.log("quarto não encontrado")
-           return
-       }
-           this.lista_de_quartos.map((quarto)=> {
-               console.log(quarto.numero ===buscaQuarto.numero)
-               if(quarto.numero === buscaQuarto.numero){
-                console.log(quarto,this.lista_de_quartos)
-                quarto.disponivel = false
-               }
-           })
-       
-    //    console.log( this.lista_de_quartos)
-
-       
+        const quarto = this.lista_de_quartos.find((quarto)=> quarto.numero === numero)
+        if(quarto === undefined){
+            console.log(`quarto não encontrado`)
+        }else if(quarto.disponivel === false){
+            console.log(quarto, "quarto encontrado e indisponivel")
+        }else{
+            console.log("quarto reservado com sucesso ")
+            quarto.disponivel = false
+            quarto.hospede = hospede
+        }
     }
+    encerrarReserva(numero){
+        const quarto = this.lista_de_quartos.find((quarto)=> quarto.numero === numero)
+        if(quarto === undefined){
+            console.log("Reserva não encerrada, quarto não encontrado")
+            return
+        }
+        if(quarto.disponivel === true){
+            console.log('quarto ja disponivel')
+            quarto.hospede.delete()
+            return
+        }
+        quarto.disponivel = true 
+        delete quarto.hospede
+
+    }
+    
 }
 
 class Quarto{
@@ -55,9 +65,15 @@ class Quarto{
 
 }
 class Hospede{
+    static lista_de_hospedes = []
     nome
     constructor(nome){
         this.nome = nome
+        this.constructor.lista_de_hospedes.push(nome)
+    }
+
+    mostrarListaHospedes(){
+        return this.constructor.lista_de_hospedes
     }
 }
 
@@ -68,19 +84,19 @@ class Reserva{
     constructor(hospede, hotel , numero){
         this.hospede = hospede
         this.hotel = hotel;
-        this.numero =numero
+        this.numero = numero
     }
-
 
     reservarQuarto(){
         this.hotel.confirmarReserva(this.numero, this.hospede)
     }
 }
 const hotel1 = new Hotel("ver a vista")
-const quartodamalda = hotel1.adicionaQuarto(new Hotel("hotel566"))
-const reserva1 = new Reserva("rod", hotel1, quartodamalda)
+const quartodamalda = hotel1.adicionaQuarto(new Quarto("CASAL", true))
+const hospede1 = new Hospede("rod")
+const reserva1 = new Reserva(hospede1.nome, hotel1, quartodamalda)
  hotel1.adicionaQuarto( new Quarto("DUPLO"))
- hotel1.adicionaQuarto( new Quarto("DUPLO",true))
+ hotel1.adicionaQuarto( new Quarto("DUPLO",false))
  hotel1.adicionaQuarto( new Quarto("DUPLO", true)) 
  hotel1.adicionaQuarto( new Quarto("DUPLO", true))
 
@@ -88,7 +104,11 @@ hotel1.adicionaQuarto( new Quarto("casal"))
 
 hotel1.adicionaQuarto( new Quarto("sla"))
 reserva1.reservarQuarto()
-
+console.log(hotel1.lista_de_quartos )
+hotel1.encerrarReserva(quartodamalda)
+// console.log(quartodamalda)
 // hotel1.adicionaQuarto(Quarto1)
 // console.log(hotel1.QuartosDisponiveis())
 // console.log(hotel1.lista_de_quartos )
+
+console.log(hospede1.mostrarListaHospedes())
